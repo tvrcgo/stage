cc6932b6b6b7862b1e46ffe9598fa0eb5e837fec
tvrcgo <tvrcgo@gmail.com>
2015-12-23 03:27:29 +0800

    optimize ~

--- jobs/currency.js
+++ jobs/currency.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var Job = require('../lib/job');
+var Job = require('../lib/job').Job,
+    range = '*/2 * * * * *';
 
 module.exports = Job(function(){
     console.log('currency...', +new Date)
-}, '*/2 * * * * *');
+}, range);
--- jobs/proxy.js
+++ jobs/proxy.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Job = require('../lib/job');
+var Job = require('../lib/job').Job;
 
 module.exports = Job(function(){
     console.log('proxy...', +new Date)
